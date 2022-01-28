3d3d253df79d6d03e6f005177ca8ef78e6f189f9
Salman Thoriq <salmanthoriq95@yahoo.com>
2022-01-29 01:44:33 +0700

    learn basic arithmetic

--- index.py
+++ index.py
@@ -1,1 +1,55 @@
-print("hello world")
+# Arithmetic
+
+## Add
+a = 10
+b = 3
+c = 10 + 3
+print(a, " + ", b, " = ", c)
+
+## Subtrac
+a = 10
+b = 3
+c = 10 - 3
+print(a, " - ", b, " = ", c)
+
+## Multipication
+a = 10
+b = 3
+c = 10 * 3
+print(a, " * ", b, " = ", c)
+
+## Divide
+a = 10
+b = 3
+c = 10 / 3
+print(a, " / ", b, " = ", c)
+
+## modulus
+a = 10
+b = 3
+c = 10 % 3
+print(a, " % ", b, " = ", c)
+
+## floor division (pembulatan angka ke bawah)
+a = 10
+b = 3
+c = 10 // 3
+print(a, " // ", b, " = ", c)
+
+## exponent
+a = 10
+b = 3
+c = 10 ** 3
+print(a, " ** ", b, " = ", c)
+
+## square root
+a = 16
+b = 1 / 2
+c = a ** b
+print("2V", a, " = ", c)
+
+# The operation priorty of artimethic following order :
+# 1. ()
+# 2. **
+# 3. *, /, %, // (if the operator in the same sentence, then the leftmost operator will calculate first) 
+# 4. +, - (if the operator in the same sentence, then the leftmost operator will calculate first) 
